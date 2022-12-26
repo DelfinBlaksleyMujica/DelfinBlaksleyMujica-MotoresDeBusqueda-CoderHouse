af1fcee6f9bc5edd97c8e843aa306afef00fb062
delfinblaksleymujica <delf.blaksley@hotmail.com>
2022-12-26 20:56:18 -0300

    Tercer commit

--- hbs/app/router/products/products.js
+++ hbs/app/router/products/products.js
@@ -3,12 +3,16 @@ const { Router } = require("express");
 
 const products = Router();
 
+const productos = [];
+
 products.get("/productos" , ( req , res )=> {
     return res.render("index")
 })
 
+
 products.post("/productos" , ( req , res )=> {
-    res.send({ message:"Anda el post"})
+    productos.push( req.body );
+    res.render("productos" , { title:"Remera 1" , price:125 , thumbnail:"www.google.com"})
 });
 
 
